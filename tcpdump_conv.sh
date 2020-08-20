adc6e51f923232aa8a88c0fdc5be1a3c24270f15
john-xor <61298039+john-xor@users.noreply.github.com>
2020-08-20 12:42:00 -0800

    Update tcpdump_conv.sh
    changed it to a one liner to avoid making any temp files

--- tcpdump_conv.sh
+++ tcpdump_conv.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-tcpdump -ddd $3 > num.txt
-lines=`cat num.txt | wc -l`
-tail -n "$((lines-7))" num.txt > num2.txt
-lines=`cat num2.txt | wc -l`
-head -n "$((lines-2))" num2.txt > num3.txt
-cat num3.txt | sed -r 's/(^(177|80|72|64)( [0-9]{1,2}){2} )([0-9]{1,10})/echo "\1$((\4-14))"/ge' > num4.txt
-echo "6 0 0 1:6 0 0 0" | tr ":" "\n" >> num4.txt
-cat num4.txt | wc -l >> num5.txt
-cat num4.txt >> num5.txt
-cat num5.txt | tr "\n" "," | sed -r "s/^/iptables -A $1 -p $2 -m bpf --bytecode \"/g" | sed -r 's/$/" -j LOG/g'
+tcpdump -ddd $3 | sed -r 's/(^(177|80|72|64)( [0-9]{1,2}){2} )([0-9]{1,10})/echo "\1$((\4-14))"/ge' | sed '2,7d' | sed '$d' | sed -re '$s/.+/6 0 0 1\n6 0 0 0/g' | sed -re '1s/(^[0-9]+)/echo "$((\1-6))"/ge' | tr "\n" "," | sed -r "s/^/sudo iptables -A $1 -p $2 -m bpf --bytecode \"/g" | sed -r 's/$/" -j LOG/g'
 echo ""
-rm -f num5.txt
-rm -f num4.txt
-rm -f num3.txt
-rm -f num2.txt
-rm -f num.txt
