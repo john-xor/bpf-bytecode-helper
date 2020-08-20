47b7c8e5db1f79114260cb4626ee05d16100339f
john-xor <61298039+john-xor@users.noreply.github.com>
2020-08-20 13:03:39 -0800

    Update tcpdump_conv.sh
    added a tag so it would be easier to configure it in rsyslog for export etc

--- tcpdump_conv.sh
+++ tcpdump_conv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tcpdump -ddd $3 | sed -r 's/(^(177|80|72|64)( [0-9]{1,2}){2} )([0-9]{1,10})/echo "\1$((\4-14))"/ge' | sed '2,7d' | sed '$d' | sed -re '$s/.+/6 0 0 1\n6 0 0 0/g' | sed -re '1s/(^[0-9]+)/echo "$((\1-6))"/ge' | tr "\n" "," | sed -r "s/^/sudo iptables -A $1 -p $2 -m bpf --bytecode \"/g" | sed -r 's/$/" -j LOG/g'
+tcpdump -ddd $4 | sed -r 's/(^(177|80|72|64)( [0-9]{1,2}){2} )([0-9]{1,10})/echo "\1$((\4-14))"/ge' | sed '2,7d' | sed '$d' | sed -re '$s/.+/6 0 0 1\n6 0 0 0/g' | sed -re '1s/(^[0-9]+)/echo "$((\1-6))"/ge' | tr "\n" "," | sed -r "s/^/sudo iptables -A $1 -p $2 -m bpf --bytecode \"/g" | sed -r "s/$/\" -j LOG --log-prefix 'iptables-$3'/g"
 echo ""
